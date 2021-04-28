6f105b4cf84de0d6ca975015d77647a347573303
James C H. Weaver <jamesweaver@jamess-mbp.home>
2021-04-28 15:21:53 -0400

    Add effects, color, and background to website. Need work on getting text over Picture.

--- final.js
+++ final.js
@@ -1,3 +1,7 @@
 document.getElementById("subtitle").addEventListener("click", () => {
     document.getElementById("subtitle").innerHTML = "The New Effect"
 });
+
+document.getElementById("title").addEventListener("click", () => {
+    document.getElementById("title").innerHTML = "I Am Iron-Man"
+});
