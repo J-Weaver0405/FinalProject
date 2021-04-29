715fe35ea0ec38cef6b9a7ff7873401ddfd53494
James C H. Weaver <jamesweaver@jamess-mbp.home>
2021-04-29 15:34:43 -0400

    New Route taken with Project

--- final.js
+++ final.js
@@ -5,3 +5,7 @@ document.getElementById("subtitle").addEventListener("click", () => {
 document.getElementById("title").addEventListener("click", () => {
     document.getElementById("title").innerHTML = "I Am Iron-Man"
 });
+
+// document.getElementById("header").addEventListener("click", () => {
+    // document.getElementById("header").style.imageRendering("https://i0.wp.com/ioshacker.com/wp-content/uploads/2015/05/Jarvis-logo.jpg")
+//})
